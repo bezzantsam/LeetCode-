eb87055e95a453526888e3b33642a955ccdf78bf
Sam Bezzant <bezzantsam@gmail.com>
2022-06-28 00:46:15 -0700

    Time: 124 ms (17.65%), Space: 45.6 MB (18.90%) - LeetHub

---
+++ 169-majority-element/169-majority-element.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function majorityElement(nums) {
+  let numsObj = {};
+  let maxFreq = 0;
+  let maxElement = null;
+for (let num of nums) {
+    numsObj[num] = numsObj[num] + 1 || 1;
+  }
+for (num in numsObj) {
+    if (numsObj[num] > maxFreq) {
+      maxFreq = numsObj[num];
+      maxElement = parseInt(num);
+    }
+  }
+  return maxElement;
+}
