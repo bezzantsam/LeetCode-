9cae066f1e0267b710450c6c10eae70b5aa63f3a
Sam Bezzant <bezzantsam@gmail.com>
2022-09-06 05:20:59 -0700

    Time: 119 ms (20.47%), Space: 44.5 MB (53.34%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.js
+++ 412-fizz-buzz/412-fizz-buzz.js
@@ -4,17 +4,16 @@
  */
 var fizzBuzz = function(n) {
        let answer = [];
-for (var i=1; i<=n; i++){
-  
-    if (i % 3  === 0 && i % 5 == 0){
-        answer.push("FizzBuzz");
-    }else if (i % 3 === 0){
-         answer.push("Fizz");
-    }else if (i % 5 === 0){
-        answer.push("Buzz");
-    }else( answer.push(i.toString()))
- 
-}
+        
+    for (var i=1; i<=n; i++){
+        let str = "";
+    if (i % 3  === 0){
+        str += ("Fizz");
+    }if (i % 5  === 0){
+         str += ("Buzz");
+    }
+     answer.push(str.length ? str : i.toString())
+    }   
 return answer;
 }
 
